0e663bbfb2d7d3f8237332aeae3f3e308ef53a22
Wojciech <wojciech.pastuszka@gazeta.pl>
2017-06-11 15:59:43 +0200

    add put delete actions in PageController

--- src/CMSBundle/Controller/PageController.php
+++ src/CMSBundle/Controller/PageController.php
@@ -44,5 +44,77 @@ class PageController extends FOSRestController
         
         return $page;
     }  
+    
+    /**
+     * @Rest\Post("/pages")
+     */
+    public function postPageAction(Request $request)
+    {
+        $page = new Page();
+
+        $title = $request->get('title');
+        $text = $request->get('text');
+        $date = $request->get('date');
+
+        if(empty($title) || empty($text) || empty($date) ){
+            return new View("Null values are not allowed", Response::HTTP_NOT_ACCEPTABLE);
+        }
+
+        $page->setTitle($title);
+        $page->setText($text);
+        $page->setDate($date);        
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($page);
+        $em->flush();
+        return new View("Page added successfully", Response::HTTP_OK);        
+    }
+    
+    /**
+    * @Rest\Put("/pages/{id}")
+    */
+    public function updateAction($id, Request $request)
+    { 
+        $em = $this->getDoctrine()->getManager();
+        
+        $repository = $this->getDoctrine()->getRepository('CMSBundle:Page');
+        $page = $repository->find($id);
+        
+        if($page === null) {
+            return new View("This page do not exist", Response::HTTP_NOT_FOUND);
+        }
+
+        $title = $request->get('title');
+        $text = $request->get('text');
+
+        $page->setTitle($title);
+        $page->setText($text);
+
+        $em->flush();
+        return new View("Page updated successfully", Response::HTTP_OK);
+    }
+
+    /**
+     * @Rest\Delete("/pages/{id}")
+    */
+    public function deleteArticleAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        
+        $repository = $this->getDoctrine()->getRepository('CMSBundle:Page');
+        $page = $repository->find($id);
+
+        if (empty($page)) {
+            return new View("The page do not exist", Response::HTTP_NOT_FOUND);
+        } else {
+            $em->remove($page);
+            $em->flush();
+        }
+        
+        return new View("Page deleted successfully", Response::HTTP_OK);
+    }
+
+    
+    
 
 }
